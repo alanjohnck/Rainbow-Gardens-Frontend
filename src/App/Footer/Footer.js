63c927f58843e88d4ca48284dc97102ab016033c
Muhammad-Aetherlink <muhammad@aetherlink.co>
2024-06-14 08:46:54 +0530

    Responsive for mobile upto 481px

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -1,98 +1,109 @@
-import React from 'react';
-import './Footer.css';
-import twitter from '../images/Twitter.svg';
-import ig from '../images/Ig.svg';
-import fb from '../images/FB.svg';
-import youTube from '../images/You tube.svg'
-import { NavLink } from 'react-router-dom';
-import mail from '../images/Mail.svg';
-import location from '../images/Location.svg';
-import phone from '../images/Phone.svg';
-import footerLine from '../images/Line 1.svg';
+import React from "react";
+import "./Footer.css";
+import twitter from "../images/Twitter.svg";
+import ig from "../images/Ig.svg";
+import fb from "../images/FB.svg";
+import youTube from "../images/You tube.svg";
+import { NavLink } from "react-router-dom";
+import mail from "../images/Mail.svg";
+import location from "../images/Location.svg";
+import phone from "../images/Phone.svg";
+import footerLine from "../images/Line 1.svg";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-        <div className='footerTopDiv'>
-          <div className='footerMainHeading'>
-            <span>Find the plants that suits you!</span>
-          </div>
+    <div className="footer">
+      <div className="footerTopDiv">
+        <div className="footerMainHeading">
+          <span>Find the plants that suit you!</span>
+        </div>
 
-          <div className='footerBottomHeading'>
-            <span>Subscribe to forager to receive monthly plant tips, store updates, promotions and more</span>
+        <div className="footerBottomHeading">
+          <span>
+            Subscribe to forager to receive monthly plant tips, store updates,
+            promotions and more
+          </span>
+        </div>
+      </div>
+
+      <div className="footerBottomDiv">
+        <div className="footerBottomLeftDiv">
+          <div className="footerBottomLeftContent">
+            <div className="footerContent">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut.
+              </span>
+            </div>
+
+            <div className="footerIconDiv">
+              <img src={twitter} alt="twitter" />
+              <img src={ig} alt="ig" />
+              <img src={fb} alt="fb" />
+              <img src={youTube} alt="youtube" />
+            </div>
           </div>
         </div>
 
-        <div className='footerBottomDiv'>
-          <div className='footerBottomLeftDiv'>
-           <div className='footerBottomLeftContent'>
-             <div className='footerContent'>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut.</span>
-             </div>
+        <div className="footerBottomRightDiv">
+          <div className="linkContainer">
+            <span>Quick Links</span>
+            <div className="linkContents">
+              <NavLink to="">
+                <span>Shop with us</span>
+              </NavLink>
 
-             <div className='footerIconDiv'>
-               <img src={twitter} alt='twitter'/>
-               <img src={ig} alt='twitter'/>
-               <img src={fb} alt='twitter'/>
-               <img src={youTube} alt='youtubr'/>
-             </div>
-           </div>
+              <NavLink to="">
+                <span>All Plants</span>
+              </NavLink>
+
+              <NavLink to="">
+                <span>About us</span>
+              </NavLink>
+
+              <NavLink to="">
+                <span>Cart</span>
+              </NavLink>
+            </div>
           </div>
 
-          <div className='footerBottomRightDiv'>
-             <div className='linkContainer'>
-                <span>Quick Links</span>
-                <div className='linkContents'>
-                    <NavLink to=''>
-                      <span>Shop with us</span>
-                    </NavLink>
+          <div className="footerContactContainer">
+            <span>Contact us</span>
+            <div className="footerContactContents">
+              <div className="footerContactContent">
+                <img src={mail} alt="mail" />
+                <span>hello@gmail.com</span>
+              </div>
 
-                    <NavLink to=''>
-                      <span>All Plants</span>
-                    </NavLink>
+              <div className="footerContactContent">
+                <img src={location} alt="mail" />
+                <span>
+                  Riverside building,County hall,
+                  <br />
+                  London SE1 7PB,
+                  <br />
+                  United Kingdom
+                </span>
+              </div>
 
-                    <NavLink to=''>
-                      <span>About us</span>
-                    </NavLink>
-
-                    <NavLink to=''>
-                      <span>Cart</span>
-                    </NavLink>
-                </div>
-             </div>
-
-             <div className='footerContactContainer'>
-               <span>Contact us</span>
-               <div className='footerContactContents'>
-                 <div className='footerContactContent'>
-                  <img src={mail} alt='mail'/>
-                  <span>hello@gmail.com</span>
-                 </div>
-
-                 <div className='footerContactContent'>
-                  <img src={location} alt='mail'/>
-                  <span>Riverside building,County hall,London SE1 7PB,United Kingdom</span>
-                 </div>
-
-                 <div className='footerContactContent'>
-                  <img src={phone} alt='mail'/>
-                  <span>451-484-5939</span>
-                 </div>
-               </div>
-             </div>
+              <div className="footerContactContent">
+                <img src={phone} alt="mail" />
+                <span>451-484-5939</span>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className='footerCcDiv'>
-         <div className='footerLineContainer'>
-          <img src={footerLine} alt='footerLine'/>
-         </div>
-
-         <div className='ccSpan'>
-            <span>Rainbow Gardens 2024</span>
-         </div>
-          
+      <div className="footerCcDiv">
+        <div className="footerLineContainer">
+          <img src={footerLine} alt="footerLine" />
         </div>
+
+        <div className="ccSpan">
+          <span>© Rainbow Gardens 2024</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
