9c8f38a6f200880cf3681ae7dc428871379443cb
SanjuMs <sanjumlp777@gmail.com>
2024-08-19 12:06:28 +0530

    footer style changed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import Product from './App/Product/Product';
 import Cart from './App/Cart/Cart';
 import Contact from './App/Contact/Contact';
 import Category from './App/Category/Category';
-<<<<<<< HEAD
 import Plants from './App/Plants/Plants';
-=======
 import AdminDashBoard from './App/Admin/Admindashboard/adminDashboard';
 import ProductAdding from './App/Admin/adminProductAdding/productAdding';
->>>>>>> admin
 
 
 function App() {
@@ -23,12 +20,9 @@ function App() {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/category' element={<Category/>}/>
-<<<<<<< HEAD
           <Route path='/plants/:plantsCategory' element={<Plants />} />
-=======
           <Route path='/adminDashboard' element={<AdminDashBoard/>}/>
           <Route path='/productAdding' element={<ProductAdding/>}/>
->>>>>>> admin
         </Routes>
       </Router>
     </div>
