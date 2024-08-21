d9a85aa7f50f34eb49b220493795adecec2edab7
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2024-08-21 12:59:35 +0530

    Thank you page created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Product from './App/Product/Product';
 import Cart from './App/Cart/Cart';
 import Contact from './App/Contact/Contact';
 import Category from './App/Category/Category';
+import ThankYou from './App/ThankYou/ThankYou';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/category' element={<Category/>}/>
+          <Route path='/thankyou' element={<ThankYou/>}/>
         </Routes>
       </Router>
     </div>
