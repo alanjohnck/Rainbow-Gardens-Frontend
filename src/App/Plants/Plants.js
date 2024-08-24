3cf88fc8e909975311cd98104f1d8614170315df
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2024-08-24 18:11:07 +0530

    travel from plant page to induvidual products fixed

--- src/App/Plants/Plants.js
+++ src/App/Plants/Plants.js
@@ -3,8 +3,8 @@ import Navbar from '../Navbar/Navbar';
 import './Plants.css';
 import Card from 'react-bootstrap/Card';
 import expandIcon from '../images/ExpandIcon.svg';
-import cardImage from '../images/CardImage.svg';
 import Footer from '../Footer/Footer';
+import {useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import indoorImg from '../images/IndoorImage.svg';
@@ -12,17 +12,23 @@ import outdoorImg from '../images/OutdoorImage.svg';
 import Airpurifier from '../images/Airpurifier.jpg';
 
 function Plants() {
-  const [selectedPlant, setSelectedPlant] = useState(null);
+  const navigate = useNavigate();
+
   const { plantsCategoryParams } = useParams();
 
   const [plantCategory, setPlantCategory] = useState('');
   const [backgroundImage, setBackgroundImage] = useState('');
   const [plantsData, setPlantsData] = useState([]); // State for API data
 //image fixing,background image fixing
+
+  const handleExpandClick = (plantId) => {
+    navigate(`/product/${plantId}`);
+  };
+
   useEffect(() => {
     const getPlantsByCategory = async () => {
       try {
-        const response = await fetch('http://localhost:3001/api/getproduct');
+        const response = await fetch(`http://localhost:3001/api/getproduct?category=${plantsCategoryParams}+Plant`);
         const data = await response.json();
         setPlantsData(data); // Assign the fetched data to plantsData state
       } catch (e) {
@@ -60,6 +66,7 @@ function Plants() {
     }
   }, [plantsCategoryParams]);
 
+
   return (
     <div className="plantsContainer">
       <div className="plantsTopDiv" style={{ backgroundImage: `url(${backgroundImage})` }}>
@@ -86,13 +93,13 @@ function Plants() {
         <div className="plantsDisplayContainer">
           <div className="PlantsGridDisplayContainer">
             {plantsData.slice(0, 9).map((plant) => (
-              <div className="plantDiv" key={plant.pno}>
+              <div className="plantCardContainer" key={plant.pno}>
                 <Card className="plantCard">
                   <Card.Img
                     variant="top"
                     src={plant.images[0]}
-                    style={{backgroundPosition:"center",backgroundSize:"cover",}}
-                    className="plantCardImage"
+                    //style={{backgroundPosition:"center",backgroundSize:"cover",}}
+                    className="plantCardImageContainer"
                   />
                   <Card.Body className="plantCardBody">
                     <div className="plantTitle">
@@ -108,7 +115,11 @@ function Plants() {
                         </div>
                       </div>
                       <div className="plantCardRightDiv">
-                        <img src={expandIcon} alt="expandIcon" />
+                        <img 
+                          src={expandIcon} 
+                          alt="expandIcon"
+                          onClick={() => handleExpandClick(plant.Pno)}
+                        />
                       </div>
                     </div>
                   </Card.Body>
